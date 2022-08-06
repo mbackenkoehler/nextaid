41ea8bb0f642fb572afabf6af1869832e0d3f7d9
Gerrit <gerritgr@users.noreply.github.com>
2022-08-06 10:47:39 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,10 @@
 cd /home/s9gtgros/nextaid/
 python train.py
+
+
+# upload results to new branch
+#echo "# Files over 49mb" >> .gitignore
+#find * -size +49M -type f -print >> .gitignore
+#NOW=$( date '+%F_%H:%M:%S' )
+#git checkout -b NOW
+#git push
