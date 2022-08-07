60e615c04a97af76279af0c7e2d0317ed0fdb591
Gerrit <gerritgr@users.noreply.github.com>
2022-08-07 12:50:37 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,11 +3,4 @@ python train.py
 
 
 # upload results to new branch
-#echo "# Files over 49mb" >> .gitignore
-#find * -size +49M -type f -print >> .gitignore
-#NOW=$( date '+%F_%H-%M-%S' )
-#git checkout -b $NOW
-#git add .gitignore
-#git add -A
-#git commit -m 'init new branch'
-#git push -u origin $NOW
+# sh ./auto_upload_git.sh
