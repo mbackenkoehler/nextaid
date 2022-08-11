02ea71046a2fab34c23e542889ec3bf0f0ec82a9
Gerrit <gerritgr@users.noreply.github.com>
2022-08-11 13:44:35 +0200

    Update auto_upload_git.sh

--- auto_upload_git.sh
+++ auto_upload_git.sh
@@ -2,9 +2,16 @@
 NOW=$( date '+%F_%H-%M-%S' )
 git checkout -b $NOW
 
-echo "# Files over 49mb" >> .gitignore
-find * -size +49M -type f -print >> .gitignore
-git add .gitignore
-git add -A
-git commit -m 'init new branch'
-git push -u origin $NOW
+
+while true; do
+  echo "# Files over 49mb" >> .gitignore
+  find * -size +49M -type f -print >> .gitignore
+  git add .gitignore
+  git add -A
+  git commit -m 'update'
+  git push -u origin $NOW
+
+  sleep $[60 * 120] # every two hours
+done
+
+
