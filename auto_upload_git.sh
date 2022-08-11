56a501f5ebe19b8eeb7ef0dd01eb5affd7355e38
Gerrit <gerritgr@users.noreply.github.com>
2022-08-11 13:39:46 +0200

    Update auto_upload_git.sh

--- auto_upload_git.sh
+++ auto_upload_git.sh
@@ -1,8 +1,9 @@
 # upload results to new branch
-echo "# Files over 49mb" >> .gitignore
-find * -size +49M -type f -print >> .gitignore
 NOW=$( date '+%F_%H-%M-%S' )
 git checkout -b $NOW
+
+echo "# Files over 49mb" >> .gitignore
+find * -size +49M -type f -print >> .gitignore
 git add .gitignore
 git add -A
 git commit -m 'init new branch'
